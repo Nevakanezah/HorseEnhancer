d6be7ba14c640f7a134b172d541ddbe80dbe5f27
Nevakanezah <770499+Nevakanezah@users.noreply.github.com>
2020-02-14 17:20:13 -0700

    Credit CLESurrealism in ArgumentParser comment

--- src/com/nevakanezah/horseenhancer/util/ArgumentParser.java
+++ src/com/nevakanezah/horseenhancer/util/ArgumentParser.java
@@ -5,6 +5,11 @@ import java.util.List;
 import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
+/**
+* @author CLESurrealism
+* Re-calculates how an array of command arguments is split, in order to enable
+* passing arguments with whitespace by enclosing them with double-quotation marks.
+**/
 public class ArgumentParser
 {
 	private ArgumentParser() {}
