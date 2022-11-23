5da81740b15449765f22b5e24cd9acebead28c15
A Flying Poro <12255407+A-Flying-Poro@users.noreply.github.com>
2022-11-24 02:41:47 +0800

    Removed plugin descriptor annotation

--- src/main/java/com/nevakanezah/horseenhancer/HorseEnhancerMain.kt
+++ src/main/java/com/nevakanezah/horseenhancer/HorseEnhancerMain.kt
@@ -10,33 +10,8 @@ import com.nevakanezah.horseenhancer.database.SQLiteDatabase
 import com.nevakanezah.horseenhancer.listener.HorseEventListener
 import org.bukkit.plugin.ServicePriority
 import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.plugin.java.annotation.command.Command
-import org.bukkit.plugin.java.annotation.dependency.Library
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion
-import org.bukkit.plugin.java.annotation.plugin.Description
-import org.bukkit.plugin.java.annotation.plugin.Plugin
-import org.bukkit.plugin.java.annotation.plugin.Website
-import org.bukkit.plugin.java.annotation.plugin.author.Author
 import java.io.File
 
-//region @ Plugin Descriptor (outdated, library does not work!)
-@Plugin(name = "HorseEnhancer", version = "2.0.0")
-@Description("All-natural horse enhancement for Minecraft")
-@Author("Nevakanezah")
-@Author("AFlyingPoro")
-@Website("https://github.com/Nevakanezah/HorseEnhancer")
-@ApiVersion(ApiVersion.Target.v1_18)
-@Command(
-    name = "horseenhancer",
-    aliases = ["he"],
-    usage = "Use '/<command> help' for a list of commands.",
-)
-// MCCoroutine
-@Library("com.github.shynixn.mccoroutine:mccoroutine-bukkit-api:2.5.0")
-@Library("com.github.shynixn.mccoroutine:mccoroutine-bukkit-core:2.5.0")
-@Library("com.zaxxer:HikariCP:5.0.1")
-@Library("org.xerial:sqlite-jdbc:3.39.3.0")
-//endregion
 class HorseEnhancerMain : JavaPlugin() {
     private lateinit var database: SQLiteDatabase
     val configHandler: ConfigHandler = ConfigHandler(this)
